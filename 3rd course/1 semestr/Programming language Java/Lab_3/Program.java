e2a7a99c7aa7b76a14eb11c941d97b9ad1f5ba81
Kyrylo Boiko <kyryl.boiko@gmail.com>
2024-10-01 02:49:39 +0300

    Java, ML and Web-technologies Labs

---
+++ 3rd course/1 semestr/Programming language Java/Lab_3/Program.java
@@ -0,0 +1,24 @@
+import java.util. *;
+
+public class Program {
+    public static void main(String[] args) {
+        int[] numbers = new int[3];
+        Scanner sc = new Scanner(System.in);
+        for (int i = 0; i < numbers.length; i++) {
+            System.out.print("Enter a number for array element " + (i + 1) + ": ");
+            numbers[i] = sc.nextInt();
+        }
+        System.out.println("The array elements are:");
+        for (int i = 0; i < numbers.length; i++) {
+            System.out.print(numbers[i] + ", ");
+        }
+        System.out.println();
+        int counter = 0;
+        for (int i = 0; i < numbers.length; i++) {
+            if (numbers[i] >= 0) {
+                counter += 1;
+            }
+        }
+        System.out.println("The number of positive elements in the array is: " + counter);
+    }    
+}
