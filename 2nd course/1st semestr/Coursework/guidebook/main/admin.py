3ee91042ce97e6370a5e5b5b4f6e523a334e50bd
Kyrylo Boiko <119179727+Ferkkkk@users.noreply.github.com>
2024-11-18 22:24:50 +0200

    Big change 2

--- 2nd course/1st semestr/Coursework/guidebook/main/admin.py
+++
@@ -1,8 +0,0 @@
-from django.contrib import admin
-from .models import Place, Event, Review, CustomUser
-# Register your models here.
-
-admin.site.register(CustomUser)
-admin.site.register(Place)
-admin.site.register(Event)
-admin.site.register(Review)
